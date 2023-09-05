6d292963fb824d53511bde9a38ea63935c773943
HASHIMWANI <136843214+HASHIMWANI@users.noreply.github.com>
2023-09-05 09:47:58 +0530

    Add files via upload

---
+++ checking_thetriangle.c
@@ -0,0 +1,26 @@
+//checking whether the triangle is  a  scalene, isosceless,  equilaterl or a right angled triangle  
+#include<stdio.h>
+int main()
+{
+	int side1,side2,side3,a,b,c;
+	printf("enter the sides of the triangle");
+	scanf("%d%d%d",&side1,&side2,&side3);
+	if(side1==side2 && side2==side3)
+	printf("the triangle is equilateral ");
+	
+	if(side1 !=side2 && side2 !=side3 && side3 !=side1)
+	printf("the triangle is  scalene");
+	if(side1!=side2 && side2!=side3 &&side3==side1)
+	printf("the triangle is a isosceless triangle ");
+	if(side1==side2 && side2 !=side3 && side3 !=side1)
+	printf("the triangle is a isosceless triangle ");
+	if(side1 !=side2 && side2==side3 && side3 !=side1)
+	printf("the triangle is a isosceless triangle ");
+	a = ( side1 * side1 ) == ( side2 * side2 ) + ( side3 * side3 ) ;
+    b = ( side2 * side2 ) == ( side1 * side1 ) + ( side3 * side3 ) ;
+    c = ( side3 * side3 ) == ( side1 * s1 ) + ( side2 * side2 ) ;
+if ( a || b || c )
+printf ( "Right-angled triangle\n" ) ;
+return 0;
+	
+}

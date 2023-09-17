fbee1a5dc78b35d6dea747bf6760c7314ad99679
HASHIMWANI <136843214+HASHIMWANI@users.noreply.github.com>
2023-09-17 17:42:43 +0530

    Update checking_thetriangle.c

--- checking_thetriangle.c
+++ checking_thetriangle.c
@@ -16,6 +16,8 @@ int main()
 	printf("the triangle is a isosceless triangle ");
 	if(side1 !=side2 && side2==side3 && side3 !=side1)
 	printf("the triangle is a isosceless triangle ");
+	if ( s1 == s2 && s2 == s3 )
+        printf ( "Equilateral triangle\n" ) ;
 	a = ( side1 * side1 ) == ( side2 * side2 ) + ( side3 * side3 ) ;
     b = ( side2 * side2 ) == ( side1 * side1 ) + ( side3 * side3 ) ;
     c = ( side3 * side3 ) == ( side1 * s1 ) + ( side2 * side2 ) ;
